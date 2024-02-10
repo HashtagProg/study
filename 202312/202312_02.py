2930f4c031d76c36b588b6f8d8b05b65cdf889a2
# <HashtagProg@gmail.com>
2024-02-10 20:39:00 +0900

    Update 202312_02.py

--- 202312/202312_02.py
+++ 202312/202312_02.py
@@ -1,2 +1,10 @@
-print('test')
-print('test')
+def solution(a, b):
+    answer = 0
+    if (a + b < b + a):
+        answer = int(str(b) + str(a))
+    else:
+        answer = int(str(a) + str(b))
+
+    print(answer)
+
+solution(9, 91)
