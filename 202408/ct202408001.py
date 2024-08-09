1e3ab37b0cee15a3752fe2f5033db7eab9894781
# <HashtagProg@gmail.com>
2024-08-09 16:51:59 +0900

    Update ct202408001.py

--- 202408/ct202408001.py
+++ 202408/ct202408001.py
@@ -1,1 +1,28 @@
-print(1)
+def solution(n):
+  answer = 0
+  # sum1 = 0
+  # sum0 = 0
+  # sum_div = 0
+  
+  # if(n % 2 == 0):
+  #   sum_div = 0
+  # else:
+  #   sum_div = 1
+
+  # while(n > 0):
+  #   if(n % 2 == 0):
+  #     sum0 += n
+  #     n -= n
+  #   if(n % 2 == 1):
+  #     sum1 += n
+  #     n -= n
+
+
+
+
+
+
+  print(999)
+  return answer
+
+solution(5)
