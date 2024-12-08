0d1f18e1376fa67012059eb4d066e0c11b7f6428
# <HashtagProg@gmail.com>
2024-12-08 22:25:51 +0900

    20241208

--- 202408/ct202408001.py
+++ 202408/ct202408001.py
@@ -1,22 +1,23 @@
 def solution(n):
   answer = 0
-  # sum1 = 0
-  # sum0 = 0
-  # sum_div = 0
+  sum1 = 0
+  sum0 = 0
+  sum_div = 0
   
-  # if(n % 2 == 0):
-  #   sum_div = 0
-  # else:
-  #   sum_div = 1
+  if(n % 2 == 0):
+    sum_div = 0
+  else:
+    sum_div = 1
 
-  # while(n > 0):
-  #   if(n % 2 == 0):
-  #     sum0 += n
-  #     n -= n
-  #   if(n % 2 == 1):
-  #     sum1 += n
-  #     n -= n
+  while(n > 0):
+    if(n % 2 == 0):
+      sum0 += n
+      n -= n
+    if(n % 2 == 1):
+      sum1 += n
+      n -= n
 
+  prį
 
 
 
