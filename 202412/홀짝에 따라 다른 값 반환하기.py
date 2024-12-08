68cfb64864a61d485a37702a53dae989336f427f
# <HashtagProg@gmail.com>
2024-12-08 23:05:21 +0900

    Create 다시 시작
    D-Day 7

---
+++ 202412/홀짝에 따라 다른 값 반환하기.py
@@ -0,0 +1,15 @@
+def solution():
+  answer = 0
+    
+  if n%2==0:
+    for i in range(1, n + 1):
+      if i%2 == 0:
+        answer += i*i
+  elif n%2==1:
+    for i in range(1, n + 1):
+      if i%2 == 1:
+        answer += i
+
+    return answer
+
+solution()
